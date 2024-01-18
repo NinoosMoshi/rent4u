ea745f6d5e2cc3c0e1c6a89b6015776cdced7c35
NinoosMoshi <ninos1357@yahoo.com>
2024-01-18 17:05:12 -0600

    init springboot and angular project

---
+++ backEnd/rent4u/src/main/java/com/ninos/Rent4uApplication.java
@@ -0,0 +1,13 @@
+package com.ninos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rent4uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rent4uApplication.class, args);
+	}
+
+}
