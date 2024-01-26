a4bdb178383c7f417f37e6d66059c0e474ca92f4
NinoosMoshi <ninos1357@yahoo.com>
2024-01-26 09:57:30 -0600

    add logout and components for admin

--- frontEnd/rent4u-ui/src/app/modules/admin/admin-routing.module.ts
+++ frontEnd/rent4u-ui/src/app/modules/admin/admin-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { GetBookingsComponent } from './components/get-bookings/get-bookings.component';
+import { SearchCarComponent } from './components/search-car/search-car.component';
+import { UpdateCarComponent } from './components/update-car/update-car.component';
+import { AddCarComponent } from './components/add-car/add-car.component';
 
 const routes: Routes = [
-  {path:'dashboard', component:AdminDashboardComponent}
+  {path:'dashboard', component:AdminDashboardComponent},
+  {path:'car', component: AddCarComponent},
+  {path:'car/:id', component: UpdateCarComponent},
+  {path:'bookings', component: GetBookingsComponent},
+  {path:'search', component: SearchCarComponent}
 ];
 
 @NgModule({
--- frontEnd/rent4u-ui/src/app/modules/admin/admin.module.ts
+++ frontEnd/rent4u-ui/src/app/modules/admin/admin.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { GetBookingsComponent } from './components/get-bookings/get-bookings.component';
+import { AddCarComponent } from './components/add-car/add-car.component';
+import { SearchCarComponent } from './components/search-car/search-car.component';
+import { UpdateCarComponent } from './components/update-car/update-car.component';
 
 
 
@@ -10,7 +14,11 @@ import { AdminDashboardComponent } from './components/admin-dashboard/admin-dash
   declarations: [
 
   
-    AdminDashboardComponent
+    AdminDashboardComponent,
+        GetBookingsComponent,
+        AddCarComponent,
+        SearchCarComponent,
+        UpdateCarComponent
   ],
   imports: [
     CommonModule,
